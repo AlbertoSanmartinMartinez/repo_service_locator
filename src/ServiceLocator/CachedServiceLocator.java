db6818139eb9cb434eba8344a1ef76f20be8e292
Juan Bone Valls <jbv9@aules>
2019-05-16 22:48:57 +0200

    ImplD

--- src/ServiceLocator/CachedServiceLocator.java
+++ src/ServiceLocator/CachedServiceLocator.java
@@ -14,43 +14,41 @@ import java.util.Map;
  */
 public class CachedServiceLocator implements ServiceLocator {
 
-    private final Map<String, Object> names;
+    private final Map<String, Object> cnames;
 
     public CachedServiceLocator () {
-        this.names = new HashMap<>();
+        this.cnames = new HashMap<>();
     }
 
     @Override
     public void setService(String name, Factory factory) throws LocatorErrors {
 
-        if (names.containsKey(name)) { // revisar si lo crea o no antes de la excepcion
+        if (cnames.containsKey(name)) { // revisar si lo crea o no antes de la excepcion
             throw new LocatorErrors();
         }
         else {
             //this.factory = new Impl1Factory();
-            //this.names.put(name, factory);
-            //
-            this.setConstant(name,factory.create(this));
+            this.cnames.put(name, factory);
         }
     }
 
     @Override
     public void setConstant(String name, Object object) throws LocatorErrors {
 
-        if (names.containsKey(name)) {
+        if (cnames.containsKey(name)) {
             throw new LocatorErrors();
         }
         else {
             //this.factory = new Impl1Factory();
-            this.names.put(name, object);
+            this.cnames.put(name, object);
         }
     }
 
     @Override
     public Object getObject(String name) throws LocatorErrors {
 
-        if (names.containsKey(name)) {
-            return names.get(name);
+        if (cnames.containsKey(name)) {
+            return cnames.get(name);
         }
         throw new LocatorErrors();
     }
---
+++ src/Test/FactoryD1.java
@@ -0,0 +1,20 @@
+
+package Test;
+
+import ServiceLocator.*;
+
+
+public class FactoryD1 implements Factory {
+
+    @Override
+    public InterfaceD create(ServiceLocator s1) throws LocatorErrors {
+
+        try {
+            int i = (int) s1.getObject("D");
+            return new ImplementationD1(i);
+        } catch (ClassCastException ex) {
+            throw new LocatorErrors();
+        }
+
+    }
+}
