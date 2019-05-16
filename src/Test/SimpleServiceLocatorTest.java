cc29dc65c86d6925020ea165268cb082f23b5b16
Juan Bone Valls <jbv9@aules>
2019-05-16 23:12:41 +0200

    Modified

--- src/Test/SimpleServiceLocatorTest.java
+++ src/Test/SimpleServiceLocatorTest.java
@@ -5,20 +5,30 @@ import ServiceLocator.*;
 import static org.junit.Assert.*;
 
 public class SimpleServiceLocatorTest {
-    public SimpleServiceLocator service = new SimpleServiceLocator();
+    private SimpleServiceLocator service = new SimpleServiceLocator();
+    private static Factory fa1;
+    private static Factory fb1;
+    private static Factory fc1;
+    private static Factory fd1;
+
+    private InterfaceC c =null;
+    private InterfaceB b =null;
+    private InterfaceD d =null;
+    private String s = "Hola";
+    private int i = 3;
+
+    private final ImplementationA1 ia1= new ImplementationA1(b,c);
+    private final ImplementationB1 ib1= new ImplementationB1(d);
+    private final ImplementationC1 ic1= new ImplementationC1(s);
+    private final ImplementationD1 id1 = new ImplementationD1(i);
 
     @Test
-    public void checkSetService() throws LocatorErrors
+    public void checkSetServiceMethods() throws LocatorErrors
     {
-        Factory fa1;
-        InterfaceC c=null;
-        InterfaceB b=null;
-        InterfaceD d=null;
-        String s1 ="Hola";
-        ImplementationB1 s= new ImplementationB1(d);
-        ImplementationC1 b1= new ImplementationC1("HI");
-        //InterfaceC b=null;
-        fa1 = new FactoryC1();
+
+
+        fa1 = new FactoryA1();
+
         service.setService("A",fa1);
         service.setConstant("B",s);
         service.setConstant("C",b1);
